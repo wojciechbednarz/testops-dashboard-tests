8d2762ce746f720433aed12c80bcc96b3002ae51
Wojciech Bednarz <bednarz.wojciech96@gmail.com>
2025-07-25 17:29:08 +0200

    Removed unnecessary '/dist' directory

--- dist/frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/main.ts
-const DashboardView_js_1 = require("./views/DashboardView.js");
-const app = document.getElementById('testList');
-if (app) {
-    (0, DashboardView_js_1.renderDashboard)(app);
-}
